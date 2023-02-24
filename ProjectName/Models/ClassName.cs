2a1ce22a18e1028577422d31524b423dd1666a8c
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-24 08:20:25 -0800

    Initial setup

--- ProjectName/Models/ClassName.cs
+++ ProjectName/Models/ClassName.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace ProjectName.Models
+namespace Bakery.Models
 {
   public class ClassName
   {
--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -1,17 +1,11 @@
 using System;
 using System.Collections.Generic;
-using ScrabbleScorer.Models;
+using Bakery.Models;
 
-namespace ScrabbleScorer
+namespace Bakery
 {
   class Program
   {
-    static void Main()
-    { // making static function in game makes not unique. doesnt change per instance. always what it does not matter what it is. only thing it takes into account is input.
-      Console.WriteLine("Enter a word to calculate your Scrabble score:");
-      string word1 = Console.ReadLine();
-      int score = ScrabbleScore.WordScore(word1);
-      Console.WriteLine($"Your Scrabble score for '{word1}' is {score}");
-    }
+    static void Main ()
   }
 }
